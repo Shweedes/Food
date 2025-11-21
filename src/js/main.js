d3ea8d53c0e082c07023282869d164e764e32dc7
Shweedes <metior22848@mail.ru>
2025-11-21 14:26:41 +0300

    add npm and json-server

--- src/js/main.js
+++ src/js/main.js
@@ -263,4 +263,9 @@ window.addEventListener('DOMContentLoaded', () => {
         }, 4000)
 
     }
+
+    fetch('http://localhost:3000/menu')
+        .then(data => data.json())
+        .then(result => console.log(result))
+
 })
